b06cf8f51dbcc70a93c5e15893bef46829b1489c
Alex Luu <103136100+LuuAlex@users.noreply.github.com>
2022-10-03 21:47:24 -0700

    rearranged files and created helper main functions

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-print("Welcome to the Casino!\n")
+import os
+import games.single_coin_slot_machine
+
+# Clears terminal screen
+def clear():
+    os.system('cls||clear')
+# Prints amount of chips player has
+def wallet_print():
+    print("Chips:", wallet)
+# Clears terminal and prints chips to reset after each round
+def reset():
+    clear()
+    wallet_print()
+
+
+print("Hello! Welcome to the Slot Machine Casino!\n")
 
 print("How many chips would you like to buy tonight?")
 wallet = float(input("Enter a dollar amount ($): "))
@@ -10,6 +25,8 @@ while True:
 
     print()
     choice = int(input("Enter the number coorspoinding to the game you would like to play:"))
+    reset()
+
 
     if choice == 1:
-        1
+        games.single_coin_slot_machine.play()
